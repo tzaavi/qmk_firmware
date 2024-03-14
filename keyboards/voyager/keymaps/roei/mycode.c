c655a9d56c090a6dff336d293b305d5772b32de9
roei <rwagner@live.com>
2024-03-13 22:52:02 -0400

    ff

--- keyboards/voyager/keymaps/roei/mycode.c
+++ keyboards/voyager/keymaps/roei/mycode.c
@@ -3,19 +3,40 @@
 // https://github.com/filterpaper/qmk_userspace
 // --------------------------------------------------------
 
+#include "print.h"
+
 // Basic keycode filter for tap-hold keys
 #define GET_TAP_KEYCODE(kc) ((kc) & 0xff)
 
 // Tap-hold decision helper macros
 #define IS_HOMEROW(r)        (r->event.key.row == 2 || r->event.key.row == 8)
-#define IS_MOD_TAP_CAG(kc)   (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LALT | QK_LGUI))
 #define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) && QK_LAYER_TAP_GET_LAYER(kc))
 #define IS_TEXT(kc)          (KC_A <= (uint8_t)(kc) && (uint8_t)(kc) <= KC_SLSH)
 #define IS_TYPING(kc)        (last_input_activity_elapsed() < QUICK_TAP_TERM && IS_TEXT(kc) && !IS_LAYER_TAP(kc))
+#define IS_MOD_TAP_SHIFT(kc) (IS_QK_MOD_TAP(kc) && (kc) & (QK_LSFT))
+#define IS_MOD_TAP_CS(kc)    (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LSFT))
+#define IS_MOD_TAP_CAG(kc)   (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LALT | QK_LGUI))
 
+// Home row mod-tap and the following key are on the same side of the keyboard
+#define IS_UNILATERAL(r, n) ( \
+    (0 <= r->event.key.row && r->event.key.row <= 4 && 0 <= n.event.key.row && n.event.key.row <= 4) || \
+    (6 <= r->event.key.row && r->event.key.row <= 10 && 6 <= n.event.key.row && n.event.key.row <= 10) )
+
+// Home row mod-tap and the following key are on opposite sides of the keyboard
+#define IS_BILATERAL(r, n) ( \
+    (r->event.key.row == 2 && 6 <= n.event.key.row && n.event.key.row <= 10) || \
+    (r->event.key.row == 8 && 0 <= n.event.key.row && n.event.key.row <= 4) )
+
+// store next key
 static uint16_t    next_keycode;
 static keyrecord_t next_record;
 
+/*
+ * Instant tap
+ * Tap-hold key-up delays can be bothersome and unnecessary while typing quickly.
+ * To eliminate these delays, the tap-hold key is replaced with its tap keycode if prior key is a text keycode
+ * and input was less than the QUICK_TAP_TERM duration in milliseconds.
+ */
 bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint16_t prev_keycode;
     static bool     is_pressed[UINT8_MAX];
@@ -42,5 +63,41 @@ bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
     }
 
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+
     return true;
+}
+
+/*
+ * Permissive bilateral hold
+ * Modifiers should be triggered when a mod-tap key is held down and another key is tapped with the opposite hand.
+ * required: #define PERMISSIVE_HOLD_PER_KEY
+ */
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    // Send Control or Shift with a nested key press on the opposite hand
+    return IS_BILATERAL(record, next_record) && IS_MOD_TAP_CS(keycode);
+}
+
+/*
+ * Stringent unilateral tap
+ * Modifiers should not be triggered when a mod-tap key is pressed in combination with another key on the same hand.
+ * required: #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+ * WARNING:this one cause mod keys to register twice for some reason, disabled for now
+ */
+bool get_hold_on_other_key_press___(uint16_t keycode, keyrecord_t *record) {
+    // Activate layer with another key press
+    if (IS_LAYER_TAP(keycode)) return true;
+
+    // Sent its tap keycode when non-Shift overlaps with another key on the same hand
+    if (IS_UNILATERAL(record, next_record)/* && !IS_MOD_TAP_SHIFT(next_keycode)*/) {
+        record->keycode = GET_TAP_KEYCODE(keycode);
+        process_record(record);
+        // Release the tap keycode and send the event
+        record->event.pressed = false;
+        process_record(record);
+    }
+
+    return false;
 }
